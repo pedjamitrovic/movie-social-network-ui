b7a6e90dafb4a21dbea203b6db60fc8b19f4f726
Predrag Mitrović <56686911+predrag-mitrovic-asseco-see@users.noreply.github.com>
2020-11-20 21:59:15 +0100

    Feat: Logged user in navigation

--- ui/src/app/components/navigation/navigation.component.ts
+++ ui/src/app/components/navigation/navigation.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { User } from '@models/user.model';
+import { UserService } from '@services/user/user.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  user: User;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService,
+    ) {}
+
+  ngOnInit(): void {
+    this.user = this.userService.generateUser();
+  }
 
 }
