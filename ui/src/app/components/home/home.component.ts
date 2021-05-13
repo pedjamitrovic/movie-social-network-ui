9138afd9dd900a3c0c862b8a7d9558057da73f57
Predrag Mitrović <56686911+predrag-mitrovic-asseco-see@users.noreply.github.com>
2021-05-13 18:08:05 +0200

    Feat: Home logo, navigation logo, login-register state

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -2,18 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@services/auth.service';
 
+enum HomeComponentState {
+  Login = 'login',
+  Register = 'register'
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  state: HomeComponentState = HomeComponentState.Login;
 
   constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
     if (this.authService.activeSystemEntityValue) {
       this.router.navigate(['/feed']);
+    }
+  }
+
+  toggleState() {
+    if (this.state === HomeComponentState.Login) {
+      this.state = HomeComponentState.Register
+    } else {
+      this.state = HomeComponentState.Login;
     }
   }
 
